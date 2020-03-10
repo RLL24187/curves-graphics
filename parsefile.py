2c353a8d57b81421ae37a14db86c83a1f8e21fc4
rleongPd5 <rleong00@stuy.edu>
2020-03-10 18:44:27 -0400

    updated parser... completed assignment

--- parsefile.py
+++ parsefile.py
@@ -37,7 +37,7 @@ The file follows the following format:
          quit: end parsing
 See the file script for an example of the file format
 """
-ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save' ]
+ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite' ]
 
 def parse_file( fname, edges, transform, screen, color ):
 
@@ -96,5 +96,14 @@ def parse_file( fname, edges, transform, screen, color ):
                 display(screen)
             else:
                 save_extension(screen, args[0])
+
+        elif line == 'circle':
+            add_circle(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), .01)
+
+        elif line == 'bezier':
+            add_curve(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]), float(args[5]), float(args[6]), float(args[7]), .01, 0)
+
+        elif line == 'hermite':
+            add_curve(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]), float(args[5]), float(args[6]), float(args[7]), .01, 1)
 
         c+= 1
